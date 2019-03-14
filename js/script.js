95e5d3daeec5f2170287ba575b962a3f50be204a
Dmitrii <dalekseev2017@gmail.com>
2019-03-14 23:40:41 +0500

    Правки по защите

--- js/script.js
+++ js/script.js
@@ -76,11 +76,15 @@ close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.add("visually-hidden");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
   if (!modalName.value || !modalEmail.value || !modalMessage.value) {
     evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
     if (!modalName.value) {
       console.log("Enter name");
       modalName.setAttribute("required", "");
@@ -112,7 +116,8 @@ window.addEventListener("keydown", function(evt) {
     if (!popup.classList.contains("modal-show")) {
       evt.preventDefault();
       popup.classList.add("visually-hidden");
-      mapPopup.classList.remove("modal-show");
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
